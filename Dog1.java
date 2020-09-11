07516a6d17abf63f173ad70ee168dc5911a086c6
yuxin <3178102653@zju.edu.cn>
2020-09-11 16:03:29 +0800

    add Dog1.java and DogLauncher.java

---
+++ Dog1.java
@@ -0,0 +1,9 @@
+public class Dog1 {
+	public static void MakeNoise(){
+		System.out.println("wowowoowowowo");
+	}
+
+	public static void main(String[] args) {
+		MakeNoise();
+	}
+}
---
+++ DogLauncher.java
@@ -0,0 +1,6 @@
+/** test dog1.java */
+public class DogLauncher {
+	public static void main(String[] args){
+		Dog1.MakeNoise();
+		}
+	}
